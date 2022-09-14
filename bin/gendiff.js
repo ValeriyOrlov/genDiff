7eb20c6e744e8e9d53a90d86cceb41c6f4480be3
Valerrriy_Orlov <ValerrriyOrlov@yandex.ru>
2022-09-14 22:29:00 +0300

    feat: update help output

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
-import { Command } from "commander";
-const program = new Command();
-
+import { program } from "commander";
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference')
-  .version('0.1.0');
-
+  .version('0.1.0')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 program.parse();
