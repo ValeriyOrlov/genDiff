b979a696bdc92a09eb0df58be48b2a6a2b0279ae
Valerrriy_Orlov <ValerrriyOrlov@yandex.ru>
2022-10-07 10:09:18 +0300

    fix lint

--- src/parsers.js
+++ src/parsers.js
@@ -1,11 +1,11 @@
-import yaml from 'js-yaml'
+import yaml from 'js-yaml';
 
 export default (file, format) => {
-    let parse;
-    if (format === '.json') {
-        parse = JSON.parse(file);
-    } else if (format === '.yml' || format === 'yaml') {
-        parse = yaml.load(file);
-    }
-    return parse;
+  let parse;
+  if (format === '.json') {
+    parse = JSON.parse(file);
+  } else if (format === '.yml' || format === 'yaml') {
+    parse = yaml.load(file);
+  }
+  return parse;
 };
