a3fe7b5702a27b6bd1d82488f10a9b179cfed8c9
Valerrriy_Orlov <ValerrriyOrlov@yandex.ru>
2022-11-01 01:49:01 +0300

    fix: little renovation done

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 import genDiff from '../src/genDiff.js';
-import parse from '../src/parsers.js';
-import readFileAndFormat from '../src/readFileAndFormat.js';
 
 program
   .name('gendiff')
@@ -11,13 +9,5 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .option('-f, --format <type>', 'output type', 'stylish')
-  .action((filepath1, filepath2) => {
-    const [data1, format1] = readFileAndFormat(filepath1);
-    const [data2, format2] = readFileAndFormat(filepath2);
-    const file1 = parse(data1, format1);
-    const file2 = parse(data2, format2);
-    genDiff(file1, file2, program.opts().format);
-  })
+  .action((filepath1, filepath2) => genDiff(filepath1, filepath2, program.opts().format))
   .parse();
-
-export default program;
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,4 +1,15 @@
 import formatSelector from '../formatters/index.js';
 import builder from './builder.js';
+import readFileAndFormat from './readFileAndFormat.js';
+import parse from './parsers.js';
 
-export default (file1, file2, formatName) => formatSelector(builder(file1, file2), formatName);
+const genDiff = (filepath1, filepath2, formatName) => {
+  const [data1, format1] = readFileAndFormat(filepath1);
+  const [data2, format2] = readFileAndFormat(filepath2);
+  const file1 = parse(data1, format1);
+  const file2 = parse(data2, format2);
+
+  return formatSelector(builder(file1, file2), formatName);
+};
+
+export default genDiff;
